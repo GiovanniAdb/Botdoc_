30e11a0596bf962fd8fac5d8cf093cd532a3f10d
GiovanniAdb <78819879+GiovanniAdb@users.noreply.github.com>
2024-02-16 21:44:18 -0600

    Componentes

---
+++ src/Componentes/Chatbot.js
@@ -0,0 +1,18 @@
+import '../css/chatBot.css'
+
+export const ChatBot = () => {
+    return (
+        <div>
+           <label form="text"> TextField: </label><input type="text"></input>
+           <label>Boton para enviar </label> <button> p </button>
+           <div class="globo_dialogo izq">
+                <p>
+                    Este es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogoEste es un ejemplo de dialogo
+                </p>
+            </div>
+            <div class="globo_dialogo izq">
+                <p>Este es un ejemplo de dialogo</p>
+            </div>
+        </div>
+    )
+}
