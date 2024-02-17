cd866be99f09ea28d4c3c3ecb6eda9b880ec4387
GiovanniAdb <78819879+GiovanniAdb@users.noreply.github.com>
2024-02-16 19:32:58 -0600

    Integracion DialogFlow

---
+++ src/DialogflowChatbot.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function DialogflowChatbot() {
+  return (
+    <iframe
+      title="Dialogflow Chatbot"
+      width="350"
+      height="430"
+      allow="microphone;"
+      src="https://console.dialogflow.com/api-client/demo/embedded/d94d1fa9-59db-4f94-94a0-081696114ded"
+    ></iframe>
+  );
+}
+
+export default DialogflowChatbot;
